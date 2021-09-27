1b51495c07d0bdadb9ec5fe4d2c54cd590ae7f01
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2021-09-28 02:59:06 +0530

    Update removebg.js

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -88,7 +88,7 @@ else if (Config.WORKTYPE == 'public') {
 		    fs.createWriteStream('rbg.png')
         );
     
-        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'WhatsAsena.png', mimetype: Mimetype.png});
+        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'Jack remove bg file.png', mimetype: Mimetype.png});
         await load.delete();
     }));
     
