10f9d6529ed01f028f5fb454486302650c3d7727
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2021-09-28 02:24:02 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Amalser*:  ```" + msg + "```"
+    return "✅ *JACK*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Amalser*:  ```" + msg + "```"
+    return "🛑 *JACK*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Amalser*:  ```" + msg + "```"
+    return "⏺️ *JACK*:  ```" + msg + "```"
 }
 
 
