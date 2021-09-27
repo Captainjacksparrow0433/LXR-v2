237d4b884d49dd866a4280840290df57386d61ba
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2021-09-28 02:40:53 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,21 +11,19 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/yCYYyEJ.jpeg";
+  r_text[0] = "https://www.linkpicture.com/q/20210926_184641.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
-*owner number wa.me/919895828468*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Abin🤗*
+*owner number wa.me/919061103727*
 
-*Bot making video : https://youtu.be/fd0KPblop-k*
+*Bot making video : https://youtu.be/2tek7Y5CEF4*
 
-*Yt_PASSWORD: _________(password on this video watch it full)*
-
-*githublink : https://github.com/cyberchekuthan/Amalser_v2*
+*githublink : https://github.com/Captainjacksparrow0433/LXR-v2*
 
 *audio commads : https://github.com/cyberchekuthan/Amalser_v2/tree/master/uploads*
 `}) 
