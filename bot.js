7a37cae1fd3f89a32586d0832d3f292273376782
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2021-10-16 15:57:08 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -150,7 +150,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
         });
 
         console.log(
-            chalk.green.bold('Jacki working 💌' + config.WORKTYPE + ' 𝚗𝚘𝚠 👻')
+            chalk.green.bold('Jacki working 💌')
             await conn.sendMessage(conn.user.jid, "Jacki working", MessageType.text);
             await conn.sendMessage(conn.user.jid, "``` NO " + config.WORKTYPE + "```" , MessageType.text);
         );
