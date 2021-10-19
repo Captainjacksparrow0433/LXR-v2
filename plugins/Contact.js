4c53b9b624fff4b0aada8b6158f2718e01df316a
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2021-10-19 10:17:11 +0530

    Update Contact.js

--- plugins/Contact.js
+++ plugins/Contact.js
@@ -14,4 +14,15 @@ const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'TEL;type=CELL;type=VOICE;waid=919061103727:+91 9061103727\n' // WhatsApp ID + phone number
             + 'END:VCARD'
 await message.client.sendMessage(message.jid,{displayname: "Abin [꧁LXR=Jack࿐]", vcard: vcard}, MessageType.contact)
+
+}));
+
+Ktb.addCommand({ pattern: 'owner ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
+const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+            + 'VERSION:3.0\n' 
+            + 'FN:Abin [꧁LXR=Jack࿐]\n' // full name
+            + 'ORG:꧁LXR=Jack࿐;\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid=919061103727:+91 9061103727\n' // WhatsApp ID + phone number
+            + 'END:VCARD'
+await message.client.sendMessage(message.jid,{displayname: "Abin [꧁LXR=Jack࿐]", vcard: vcard}, MessageType.contact)
 }))
