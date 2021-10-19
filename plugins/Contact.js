d256a1abf73c2ac3043989e1aab6ef09b45ccfb9
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2021-10-19 06:22:24 +0530

    Create Contact.js

---
+++ plugins/Contact.js
@@ -0,0 +1,17 @@
+const Ktb = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const exec = require('child_process').exec;
+const os = require("os");
+const fs = require('fs');
+const Config = require('../config')
+Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
+
+//coded by saidali
+const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+            + 'VERSION:3.0\n' 
+            + 'FN:Abin [꧁LXR=Jack࿐]\n' // full name
+            + 'ORG:꧁LXR=Jack࿐;\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid=919061103727:+91 9061103727\n' // WhatsApp ID + phone number
+            + 'END:VCARD'
+await message.client.sendMessage(message.jid,{displayname: "Abin [꧁LXR=Jack࿐]", vcard: vcard}, MessageType.contact)
+}))
