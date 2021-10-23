40b03285c24fb0eb72bd37f0ab6c333b7e8b5272
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2021-10-23 11:54:01 +0530

    Update farhanamal.js

--- plugins/farhanamal.js
+++ plugins/farhanamal.js
@@ -1,8 +1,8 @@
-/* Copyright (C) 2021 JACKI.
+/* Copyright (C) 2021 AMALSER.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-Jacki - Abin
-Wa.me/+919061103727
+Amalser - Amal
+Wa.me/+919895828468
 */
 
 const Neotro = require('../events');
