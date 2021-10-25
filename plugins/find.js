ed32644ae808528dedb0e11672115ece5ec86202
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2021-10-25 22:15:10 +0530

    Update find.js

--- plugins/find.js
+++ plugins/find.js
@@ -12,64 +12,30 @@ const ffmpeg = require('fluent-ffmpeg');
 
 const FIND_DESC = "Finds the Song"
 
-Asena.addCommand({pattern: 'find', fromMe: false, desc: FIND_DESC }, (async (message, match) => {
-
-    if (message.reply_message === false) return await message.client.sendMessage(message.jid, '*audio reply aayitt koduk!', MessageType.text);
-
-    var filePath = await message.client.downloadAndSaveMediaMessage({
-
-        key: {
-
-            remoteJid: message.reply_message.jid,
-
-            id: message.reply_message.id
-
-        },
-
-        message: message.reply_message.data.quotedMessage
-
-    });
-
-    var form = new FormData();
-
-    ffmpeg(filePath).format('mp3').save('music.mp3').on('end', async () => {
-
-        form.append('api_token', '2bd1fcb3ccd15607f72cdbb527907ce6');
-
-        form.append('file', fs.createReadStream('./music.mp3'));
-
-        form.append('return', 'apple_music, spotify');
-
-        var configs = {
-
-            headers: {
-
-                ...form.getHeaders()
-
-            }
-
-        }
-
-        await axios.post('https://api.audd.io/', form, configs).then(async (response) => {
-
-            var res = response.data
-
-            if (res === 'success') {
-
-                await message.client.sendMessage(message.jid, `Title: ${res.title}\nArtist: ${res.artist}`, MessageType.text);
-
-            } else {
-
-                await message.client.sendMessage(message.jid, 'No results found', MessageType.text);
-
-            }
-
-        }).catch((error) =>  {
-
-            console.log(error);
-
-        });
-
-    });
-
-}));
+Asena.addCommand(
+  { pattern: "find", fromMe: true, desc: Lang.FIND_DESC },
+  async (message, match) => {
+    if (
+      !message.reply_message ||
+      (!message.reply_message.audio && !message.reply_message.video)
+    )
+      return await message.sendMessage(Lang.FIND_NEED_REPLY);
+    let location = await message.reply_message.downloadAndSaveMediaMessage(
+      "find"
+    );
+    let buff = await audioCut(location, 0, 15, "findo");
+    const data = await IdentifySong(buff);
+    if (!data) return;
+    if (!data.status) return await message.sendMessage(Lang.NOT_FOUND);
+    return await message.sendMessage(
+      Lang.FIND_MSG.format(
+        data.data.title,
+        data.data.artists,
+        data.data.genre,
+        data.data.album,
+        data.data.release_date
+      ),
+      { quoted: message.quoted }
+    );
+  }
+);
