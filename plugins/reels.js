94bda28bb088e42425e6a85ca85264b10bdfe061
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2021-10-27 21:32:32 +0530

    Update reels.js

--- plugins/reels.js
+++ plugins/reels.js
@@ -10,6 +10,7 @@ const axios = require ('axios')
 const LOAD_ING = "*Connecting To instagram*"
 const UPLOAD_ING = "*Connected To instagram* \n\n\n *Downloading & uploading in process...*"
 const instagram = async (url, key) => {
+const { getBuffer, igStory, downVideo } = require("../download");
     const _0x4a94a8 = _0x185a; function _0x3f3b() { const _0x37037c = ['jul', 'htt', 'ps:', 'nti', 'ouy', 'aw.', 'kit', '/in', 'sta', '?ur', 'dat', 'get', 'arr']; _0x3f3b = function () { return _0x37037c; }; return _0x3f3b(); } function _0x185a(_0x38e93d, _0x3f3b83) { const _0x185a5f = _0x3f3b(); _0x185a = function (_0x829ec5, _0x405d60) { _0x829ec5 = _0x829ec5 - 0xe5; let _0x20f676 = _0x185a5f[_0x829ec5]; return _0x20f676; }; return _0x185a(_0x38e93d, _0x3f3b83); } if (key != _0x4a94a8(0xe5) + 'ie') throw new Error('Jul' + 'ie'); const response = await axios(_0x4a94a8(0xe6) + _0x4a94a8(0xe7) + '//u' + _0x4a94a8(0xe8) + 'tle' + 'd-1' + _0x4a94a8(0xe9) + 'r1r' + 'szh' + _0x4a94a8(0xea) + 'run' + _0x4a94a8(0xeb) + '.sh' + _0x4a94a8(0xec) + _0x4a94a8(0xed) + _0x4a94a8(0xee) + 'l=' + url); const { status, result } = response[_0x4a94a8(0xef) + 'a']; if (!status) return { 'status': status }; const { type, data } = result[0x0]; const res = await axios[_0x4a94a8(0xf0)](data, { 'responseType': _0x4a94a8(0xf1) + 'ayb' + 'uff' + 'er' }); return { 'type': type, 'data': res[_0x4a94a8(0xef) + 'a'], 'status': status };
 }
 Asena.addCommand({pattern: 'insta ?(.*)', fromMe: false, desc: "Downloads from instagaram" , dontAddCommandList: true }, async (message, match) => {
@@ -28,3 +29,40 @@ Asena.addCommand({pattern: 'insta ?(.*)', fromMe: false, desc: "Downloads from i
 });
     
 // thanks to :- farhan-dqz
+
+Asena.addCommand({ pattern: 'story ?(.*)', fromMe: true, desc: "Download stories of given username\n.story username or reply to a username or story url"},
+  async (message, match) => {
+    match = !message.reply_message ? match : message.reply_message.text;
+    if (
+      match === "" ||
+      (!match.includes("/stories/") && match.startsWith("http"))
+    )
+      return await message.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
+    if (match.includes("/stories/")) {
+      let s = match.indexOf("/stories/") + 9;
+      let e = match.lastIndexOf("/");
+      match = match.substring(s, e);
+    }
+    let json = await igStory(match);
+    if (json.error) return await message.sendMessage(json.error);
+    if (json.medias.length > 0) {
+      await message.sendMessage(message.jid,UPLOAD_ING , MessageType.text, { quoted: message.data });
+      );
+      for (let media of json.medias) {
+        let { buffer, type } = await getBuffer(media.url);
+        if (type == "video")
+          await message.sendMessage(
+            buffer,
+            { mimetype: Mimetype.mp4 },
+            MessageType.video
+          );
+        else if (type == "image")
+          await message.sendMessage(
+            buffer,
+            { mimetype: Mimetype.jpeg },
+            MessageType.image
+          );
+      }
+    }
+  }
+);
