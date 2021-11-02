58341b5f8237859669319c2fba1f437beb3d35e2
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2021-11-02 16:51:21 +0530

    Update StringSession.js

--- julie/StringSession.js
+++ julie/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'Jacki;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'Amalser;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
