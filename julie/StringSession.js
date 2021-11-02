78407144ecfc873a4cf00299d213745a82a2b765
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2021-11-02 17:36:03 +0530

    Update StringSession.js

--- julie/StringSession.js
+++ julie/StringSession.js
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('JACKI_QR' in process.env && string === undefined) {
+        if ('AMALSER_CODE' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
