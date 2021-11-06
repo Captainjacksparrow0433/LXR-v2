65d6b729b16a5760e6aa39ae674f6fd31e455f1f
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2021-11-06 19:31:23 +0530

    Added emoji Gopalan 😂😂😂😂

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2021 Jacki.
+/* Copyright (C) 2021 Jacki bot.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 Remoded by - Abin
