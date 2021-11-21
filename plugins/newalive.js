ca6b094495b0c01f03dde2eb8a1f9b40da352fdb
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2021-11-21 20:04:06 +0530

    Time added ✅

--- plugins/newalive.js
+++ plugins/newalive.js
@@ -19,13 +19,17 @@ let whb = Config.WORKTYPE == 'public' ? false : true
 
 Jacki.addCommand({pattern: 'alive', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
 // send a buttons message!credit Abin
+    var plk_say = new Date().toLocaleString('HI', { timeZone: 'Asia/Kolkata' }).split(' ')[1]
+const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var plk_here = new Date().toLocaleDateString(get_localized_date)
+var afnplk = '```⏱ Time :' + plk_say + '```\n\n ```📅 Date :' + plk_here + '````
     const buttons = [
         {buttonId: 'id1', buttonText: {displayText: Config.ABIN_SER }, type: 1},
         {buttonId: 'id2', buttonText: {displayText: Config.ABIN_SIR }, type: 1}
       ]
       
       const buttonMessage = {
-          contentText: '```'+Config.BOT+'\n\n```'+Config.ALIVEMSG+'\n',
+          contentText: '```'+Config.BOT+'\n\n```'+Config.ALIVEMSG+'\n\n\n\n🃏 ᴛɪᴍᴇ   : ```' + plk_say + '```\n🍒 ᴅᴀᴛᴇ : ```' + plk_here + '```',
           footerText: 'Abin ©',
           buttons: buttons,
           headerType: 1
