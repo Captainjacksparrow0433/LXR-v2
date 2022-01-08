cb42adeaecc712e1b985900197baa06f6e5f63ee
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2022-01-08 17:38:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -62,6 +62,7 @@ module.exports = {
     UNBLOCKMSG: process.env.UNBLOCK_MESSAGE === undefined ? 'default' : process.env.UNBLOCK_MESSAGE,
     UNMUTEMSG: process.env.UNMUTE_MESSAGE === undefined ? 'default' : process.env.UNMUTE_MESSAGE,
     GEAR: process.env.CHANGE_BGM_TO === undefined ? 'one' : process.env.CHANGE_BGM_TO,
+    ALIVETYPE: process.env.ALIVE_TYPE === undefined ? 'one' : process.env.ALIVE_TYPE,
     WORKTYPE: process.env.WORK_TYPE === undefined ? 'public' : process.env.WORK_TYPE,
     PROMOTEMSG: process.env.PROMOTE_MESSAGE === undefined ? 'default' : process.env.PROMOTE_MESSAGE,
     DEMOTEMSG: process.env.DEMOTE_MESSAGE === undefined ? 'default' : process.env.DEMOTE_MESSAGE,
